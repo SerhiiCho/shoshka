31b2bc20dceab328293c0b951afb96112c351cee
SerhiiCho <serhiicho@protonmail.com>
2019-09-27 19:34:05 +0300

    Added sendMessageIfNewReports method to Register class

--- app/Register.php
+++ app/Register.php
@@ -13,4 +13,26 @@ final class Register
 
         return $this;
     }
+
+    public function sendMessageIfNewReports(): self
+    {
+        $bot = new Bot;
+        $cache = new Cache;
+        $say_cheese = new SayCheese($cache);
+        $new_items = $say_cheese->getNewItems();
+
+        if (!$new_items || empty($new_items)) {
+            exit(1);
+        }
+
+        foreach ($new_items as $result) {
+            $title = $result['titles'];
+            $link = $result['links'];
+
+            $bot->sendMessage((int) getenv('BOT_CHAT_ID'), $bot->getWelcomeMessage($title, $link));
+            $cache->putNewItem($title);
+        }
+
+        return $this;
+    }
 }
--- bootstrap.php
+++ bootstrap.php
@@ -1,28 +1,11 @@
 <?php declare(strict_types=1);
 
-use App\Bot;
 use App\Register;
-use App\SayCheese;
-use App\Cache;
 
 require_once 'vendor/autoload.php';
 require_once 'constants.php';
 
-(new Register)->registerEnvPackage();
+(new Register)
+    ->registerEnvPackage()
+    ->sendMessageIfNewReports();
 
-$bot = new Bot;
-$cache = new Cache;
-$say_cheese = new SayCheese($cache);
-$new_items = $say_cheese->getNewItems();
-
-if (!$new_items || empty($new_items)) {
-    exit(1);
-}
-
-foreach ($new_items as $result) {
-    $title = $result['titles'];
-    $link = $result['links'];
-
-    $bot->sendMessage((int) getenv('BOT_CHAT_ID'), $bot->getWelcomeMessage($title, $link));
-    $cache->putNewItem($title);
-}
