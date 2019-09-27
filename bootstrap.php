cf1b7ca3059e18d73b606b6eb90fb5de56c79528
SerhiiCho <serhiicho@protonmail.com>
2019-09-27 19:30:13 +0300

    Changed bootstrap.php file

--- bootstrap.php
+++ bootstrap.php
@@ -15,13 +15,14 @@ $cache = new Cache;
 $say_cheese = new SayCheese($cache);
 $new_items = $say_cheese->getNewItems();
 
-if ($new_items && !empty($new_items)) {
-    foreach ($new_items as $result) {
-        $title = $result['titles'];
-        $link = $result['links'];
-
-        $bot->sendMessage((int) getenv('BOT_CHAT_ID'), $bot->getWelcomeMessage($title, $link));
-        $cache->putNewItem($title);
-    }
+if (!$new_items || empty($new_items)) {
+    exit(1);
 }
 
+foreach ($new_items as $result) {
+    $title = $result['titles'];
+    $link = $result['links'];
+
+    $bot->sendMessage((int) getenv('BOT_CHAT_ID'), $bot->getWelcomeMessage($title, $link));
+    $cache->putNewItem($title);
+}
